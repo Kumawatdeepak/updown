7b528388bf1efd93edb4133c4180bbb37a548ee4
Deepak <deepakkumawat8993@gmail.com>
2021-06-10 15:28:20 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import "./App.css"
+import {useSelector,useDispatch} from 'react-redux'; 
+import {incNumber,decNumber} from './actions/index'; 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+   const mystate = useSelector(state => state.changeTheNumber);
+    console.log('state= ', mystate)
+   const dispatch = useDispatch();
+  return <>
+    <div className="container">
+      <h1>Welcome To Redux </h1>
+      <h4>Using react and Redux </h4>
+      <button className="buttons" onClick={()=>dispatch(decNumber())}>-</button><input type="text" className="inputs" value={mystate}></input><button className="buttons" onClick={()=>dispatch(incNumber())}>+</button>
     </div>
-  );
+  </>
 }
-
-export default App;
+ 
+export default App
